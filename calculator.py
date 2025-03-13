065179df9f25b0ae7ffaa4b242e5eca43592ee98
username-404-rgb <alexander.adams4@education.nsw.gov.au>
2025-03-13 14:46:27 +1100

    Calculator

---
+++ calculator.py
@@ -0,0 +1,9 @@
+from math_operations import *
+def main():
+    num_1 = int(input('Enter first number: '))
+    num_2 = int(input('Enter second number: '))
+
+    final = add_numbers(num_1, num_2)
+    print(f'Sum of two numbers is {final}!')
+
+main()
---
+++ math_operations.py
@@ -0,0 +1,3 @@
+def add_numbers(a, b):
+    total = a + b
+    return total
