df429e8dfa987e9cb5605837cdd69ae335221014
username-404-rgb <alexander.adams4@education.nsw.gov.au>
2026-02-27 09:58:14 +1100

    defensive-data-input-handling

--- name_verif.py
+++ name_verif.py
@@ -4,20 +4,28 @@ def sanitize():
     user_input = input("Enter your username: ")
     sanitized = re.sub(r'<script.*?>.*?</script>', '', user_input, flags=re.IGNORECASE | re.DOTALL)
     sanitized = sanitized.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
-    print (sanitized)
-    
-def valid_name():
-    try:
-        username = input("Enter your username: ")
-        if 0 <= age_input <= 120:
-            print('Your username has been set to', username)
-            quit()
+    username = sanitized
+    if len(username) <= 15:
+        if len(username) >= 3:
+            if username.isalnum() == True:
+                print(f"Welcome, {username}!")
+            else:
+                print('Only use letters and numbers')
         else:
-            raise ValueError("Age must be between 0 and 120.")
-    except ValueError as e:
-        print(f"Invalid input: {e}")
-        return None
+            print('Too Short')
+    else:
+        print('Too Long')
+    
+def valid_name(username):
+    if len(username) <= 15:
+        if len(username) >= 3:
+            if username.isalnum() == True:
+                print(username, "!!!")
+            else:
+                print('Only use letters and numbers')
+        else:
+            print('Too Long')
+    else:
+        print('Too Short')
+    
 sanitize()
-
-#print(f"Welcome, {username}!")
-
